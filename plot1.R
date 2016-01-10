3ba05d6f56821d0d3058651d5a08e8fa31bba40f
Kairat <kairat.other@gmail.com>
2016-01-10 20:41:17 +0600

    1st is Done

---
+++ plot1.R
@@ -0,0 +1,11 @@
+#d = read.table("data_.txt", header = TRUE, sep = ";", na.string = "?")
+#startDate = as.Date("2007-02-01")
+#endDate = as.Date("2007-02-02")
+#dd <- subset(d, as.Date(d$Date, "%d/%m/%Y") >= startDate & as.Date(d$Date, "%d/%m/%Y") <= endDate)
+
+d = read.table("data_.txt", header = TRUE)
+hist(as.numeric(d$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot1.png")
+dev.off()
+
